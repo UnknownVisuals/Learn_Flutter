92b8cea744ec488ed424b33e8e514cbcdd39d079
Reynaldhi <reynaldig40@gmail.com>
2025-02-07 00:23:12 +0700

    initialize project

---
+++ lib/profile_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatefulWidget {
+  const ProfileCard({super.key});
+
+  @override
+  State<ProfileCard> createState() => _ProfileCardState();
+}
+
+class _ProfileCardState extends State<ProfileCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
